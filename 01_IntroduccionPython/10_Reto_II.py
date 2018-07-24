d4befd85f69b025786c12aafd5bc97d09aee8f8c
iNouvellie <r.rocuant4@hotmail.com>
2018-07-24 05:05:53 -0400

    Reto II: Completado

---
+++ 01_IntroduccionPython/10_Reto_II.py
@@ -0,0 +1,9 @@
+#Pedir 2 numeros y entregar la suma
+
+primer_numero = input ("Ingrese un numero: ")
+segundo_numero = input ("Ingrese otro numero: ")
+#Podria ser casteado al ingresar
+#primer_numero = int(input ("Ingrese un numero: "))
+
+print ("\n")
+print ("La suma de esos 2 numeros es: %i" %  (int(primer_numero) + int(segundo_numero))) 
