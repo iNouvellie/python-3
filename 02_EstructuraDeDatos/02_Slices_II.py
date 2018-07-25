9a9b4735dda5c675da1f702f0ef2a913ae184e53
iNouvellie <r.rocuant4@hotmail.com>
2018-07-24 21:33:51 -0400

    Steeps en Slices

---
+++ 02_EstructuraDeDatos/02_Slices_II.py
@@ -0,0 +1,18 @@
+#Steeps
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#Con : muestra todo, y con el segundo : mas el 2, hace de 2 en 2
+print (numeros[::2])
+
+#Imprime todo de 3 en 3
+print (numeros[::3])
+
+#Del indice 2 al 7 de 2 en 2
+print (numeros[2:8:2])
+
+#Con indices negativos, con -1 invierte la lista
+print (numeros[::-1])
+
+#Muestra del penultimo hasta el indice 5 desde derecha a izquierda
+print (numeros[-2:-5:-1])
